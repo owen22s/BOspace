737388d6190f0b6cdbe0459421bbc1866a311d46
owen22s <34884@ma-web.nl>
2023-05-10 11:54:50 +0100

    hp script

--- SpaceGameTeam12/Assets/scripts/Attractor.cs
+++ SpaceGameTeam12/Assets/scripts/Attractor.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+
 public class Attractor : MonoBehaviour
 {
     public Rigidbody rb;
     private void FixedUpdate()
     {
-        Attractor[] attractors = FindObjectsOfType<Attractor>();
-        foreach (Attractor attractor in attractors) 
+        GameObject[] gravityObjects = GameObject.FindGameObjectsWithTag("Gravity");
+        foreach (GameObject gravityObject in gravityObjects)
         {
-            if (attractor != this)
-            Attract(attractor);
+            Attractor attractor = gravityObject.GetComponent<Attractor>();
+            if (attractor != null && attractor != this)
+            {
+                Attract(attractor);
+            }
         }
     }
 
-    void Attract(Attractor objToAttract) 
+    void Attract(Attractor objToAttract)
     {
         Rigidbody rbToAttract = objToAttract.rb;
 
         Vector3 direction = rb.position - rbToAttract.position;
 
-        float distance = direction.magnitude; 
+        float distance = direction.magnitude;
 
-        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance , 2);
+        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
 
         Vector3 force = direction.normalized * forceMagnitude;
 

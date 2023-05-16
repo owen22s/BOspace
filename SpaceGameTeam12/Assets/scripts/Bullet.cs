7701e3b78451a19ef70891640c6178a278778e60
owen22s <34884@ma-web.nl>
2023-05-16 10:53:27 +0100

    shooting script & movement update

--- SpaceGameTeam12/Assets/scripts/Bullet.cs
+++ SpaceGameTeam12/Assets/scripts/Bullet.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private Vector3 dir = Vector3.zero;
-    private float speed = 0;
-    void Update()
-    {
+    public float speed = 10f;
+    public float lifetime = 3f;
 
-        transform.position += dir * speed * Time.deltaTime;
+    private void Start()
+    {
+        // Destroy the bullet after the specified lifetime
+        Destroy(gameObject, lifetime);
     }
 
-    public void Init(Vector3 dir, float speed)
+    private void Update()
     {
-        this.dir = dir;
-        this.speed = speed;
-        transform.right = dir;
+        // Move the bullet forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the bullet collided with an object tagged as the targetTag
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
+
---
+++ SpaceGameTeam12/Assets/scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // Create a new bullet instance
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Get the Rigidbody component of the bullet
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+
+        // Apply force to the bullet to shoot it forward
+        bulletRb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+    }
+}
+

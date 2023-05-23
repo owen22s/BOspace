89db3640742b5f259a22ea4572da39fa61231bf4
owen22s <34884@ma-web.nl>
2023-05-23 12:00:49 +0100

    sound fx voor wapen

--- SpaceGameTeam12/Assets/scripts/Weapon.cs
+++ SpaceGameTeam12/Assets/scripts/Weapon.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 10f;
+    public AudioSource lasereffect;
+
+    private bool canShoot = true;
+    private float shootDelay = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canShoot)
         {
             Shoot();
         }
@@ -19,12 +25,15 @@ public class Weapon : MonoBehaviour
     {
         // Create a new bullet instance
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        lasereffect.Play();
 
-        // Get the Rigidbody component of the bullet
-        //Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        StartCoroutine(ShootDelay());
+    }
 
-        // Apply force to the bullet to shoot it forward
-        //bulletRb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+    IEnumerator ShootDelay()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootDelay);
+        canShoot = true;
     }
 }
-

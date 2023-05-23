531bd8f7add15f52d0c8729749338748bf1ca062
owen22s <34884@ma-web.nl>
2023-05-23 08:51:13 +0100

    spikes toegevoegd

--- SpaceGameTeam12/Assets/scripts/hp.cs
+++ SpaceGameTeam12/Assets/scripts/hp.cs
@@ -8,7 +8,12 @@ public class hp : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Spikes")) 
+        {
+            health -= 1;
+            Debug.Log(health);
+        }
+            if (collision.gameObject.CompareTag("Enemy"))
         {
             health -= 1;
             Debug.Log("Health reduced! Current health: " + health);
@@ -32,6 +37,17 @@ public class hp : MonoBehaviour
             health -= 2;
             Debug.Log("Health reduced! Current health: " + health);
         }
+        void OnTriggerEnter(Collider other) 
+        {
+            if (collision.gameObject.CompareTag("Spikes"))
+            {
+                health -= 1;
+                Debug.Log(health);
+            }
+        }
+
+
+
         if (health <= 0)
         {
             // Do something when health is zero or negative
